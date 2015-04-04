7fd765a8aaf4b754d446f66d1bea99436b7a2fb7
Amanda Gilmore <ms.amgilmore@gmail.com>
2015-04-04 15:30:33 -0700

    removed the localhost SSL cert context

--- bioretro.py
+++ bioretro.py
@@ -139,9 +139,9 @@ PORT=int(os.environ.get("PORT", 8100))
 # TODO Update the SSL context with the correct credentials for Heroku
 if __name__ == '__main__': 
 	app.run( 
-		port=PORT, 
-		ssl_context=('/Users/amandagilmore/GoogleFit/server.crt', 
-			'/Users/amandagilmore/GoogleFit/server.key'))
+		port=PORT))
+
+		# ssl_context=('/Users/amandagilmore/GoogleFit/server.crt', '/Users/amandagilmore/GoogleFit/server.key'
 
 
 
