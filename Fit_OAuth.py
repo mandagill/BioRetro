7d4d4cfd036275f5e1f010164079613a9620cd4b
Amanda Gilmore <ms.amgilmore@gmail.com>
2015-04-06 14:36:33 -0700

    Uncommented OAuth2 integration to turn API calls back on

--- Fit_OAuth.py
+++ Fit_OAuth.py
@@ -1,97 +1,97 @@
-# """ This contains code to authorize the Google Fit API with OAuth, create a session, 
-# and retrieve protected data with the resulting session token."""
+""" This contains code to authorize the Google Fit API with OAuth, create a session, 
+and retrieve protected data with the resulting session token."""
 
-# from requests_oauthlib import OAuth2Session
-# from flask import Flask, request, redirect, session, url_for, render_template
-# from flask.json import jsonify
-# import os
-
-
-# FIT_CLIENT_ID = os.environ['FIT_CLIENT_ID']
-# FIT_CLIENT_SECRET = os.environ['FIT_CLIENT_SECRET']
-# # Hardcoding to localhost until I can sort out deployment
-# REDIRECT_URI = 'https://localhost:8100/callback'
-
-# # These are the OAuth enpoints in the Google Fit API documentation: 
-# # https://developers.google.com/accounts/docs/OAuth2WebServer
-# AUTHORIZATION_BASE_URL = 'https://accounts.google.com/o/oauth2/auth'
-# TOKEN_URL = 'https://accounts.google.com/o/oauth2/token'
-# SCOPE = [
-# 	'https://www.googleapis.com/auth/fitness.body.read',
-# 	'https://www.googleapis.com/auth/fitness.location.read'
-# ]
-
-# # These are the required parameters to call the Google Fit API. The base URL is 
-# # self-explanatory, and the foo_DATA constants are data stream IDs from which
-# # we can fetch data.
-# QUERY_BASE_URI = 'https://www.googleapis.com/fitness/v1/users/me/dataSources'
-# SPEED_DATA = '/derived:com.google.speed:com.google.android.gms:merge_speed'
-# BPM_DATA = '/derived:com.google.heart_rate.bpm:com.google.android.gms:merge_heart_rate_bpm'
+from requests_oauthlib import OAuth2Session
+from flask import Flask, request, redirect, session, url_for, render_template
+from flask.json import jsonify
+import os
 
 
-# def go_to_google():
-# 	"""Redirects to the OAuth provider. Putting inside the home function per the tutorial here: 
-# 	https://requests-oauthlib.readthedocs.org/en/latest/examples/real_world_example.html#real-example"""
+FIT_CLIENT_ID = os.environ['FIT_CLIENT_ID']
+FIT_CLIENT_SECRET = os.environ['FIT_CLIENT_SECRET']
+# Hardcoding to localhost until I can sort out deployment
+REDIRECT_URI = 'https://localhost:8100/callback'
 
-# 	google = OAuth2Session(FIT_CLIENT_ID, scope=SCOPE, redirect_uri=REDIRECT_URI)
-# 	authorization_url, state = google.authorization_url(AUTHORIZATION_BASE_URL,
-# 		access_type='offline',
-# 		approval_prompt='force')
+# These are the OAuth enpoints in the Google Fit API documentation: 
+# https://developers.google.com/accounts/docs/OAuth2WebServer
+AUTHORIZATION_BASE_URL = 'https://accounts.google.com/o/oauth2/auth'
+TOKEN_URL = 'https://accounts.google.com/o/oauth2/token'
+SCOPE = [
+	'https://www.googleapis.com/auth/fitness.body.read',
+	'https://www.googleapis.com/auth/fitness.location.read'
+]
 
-# 	session['auth_url'] = authorization_url
-# 	session['oauth_state'] = state
-
-# 	return authorization_url
-
-
-# def callback(authcode):
-# 	"""Retrieving an access token."""
-
-# 	google = OAuth2Session(FIT_CLIENT_ID, state=session['oauth_state'], redirect_uri=REDIRECT_URI)
-
-# 	token = google.fetch_token(
-# 		TOKEN_URL,
-# 		client_id = FIT_CLIENT_ID,
-# 		client_secret = FIT_CLIENT_SECRET,
-# 		code=authcode )
-
-# 	session['oauth_token'] = token
-
-# 	return render_template('okauth.html')
+# These are the required parameters to call the Google Fit API. The base URL is 
+# self-explanatory, and the foo_DATA constants are data stream IDs from which
+# we can fetch data.
+QUERY_BASE_URI = 'https://www.googleapis.com/fitness/v1/users/me/dataSources'
+SPEED_DATA = '/derived:com.google.speed:com.google.android.gms:merge_speed'
+BPM_DATA = '/derived:com.google.heart_rate.bpm:com.google.android.gms:merge_heart_rate_bpm'
 
 
-# def fetch_data(startbound, endbound, data_type):
-# 	"""Take a parameter from the caller as to what kind of data is desired,
-# 	and start/end time parameters (in nanoseconds, since the Google API was clearly 
-# 	written by Java programmers) for the desired dataset.
+def go_to_google():
+	"""Redirects to the OAuth provider. Putting inside the home function per the tutorial here: 
+	https://requests-oauthlib.readthedocs.org/en/latest/examples/real_world_example.html#real-example"""
 
-# 	pass string 'speed' for speed data.
-# 	pass string 'bpm' for bpm data. """
+	google = OAuth2Session(FIT_CLIENT_ID, scope=SCOPE, redirect_uri=REDIRECT_URI)
+	authorization_url, state = google.authorization_url(AUTHORIZATION_BASE_URL,
+		access_type='offline',
+		approval_prompt='force')
+
+	session['auth_url'] = authorization_url
+	session['oauth_state'] = state
+
+	return authorization_url
+
+
+def callback(authcode):
+	"""Retrieving an access token."""
+
+	google = OAuth2Session(FIT_CLIENT_ID, state=session['oauth_state'], redirect_uri=REDIRECT_URI)
+
+	token = google.fetch_token(
+		TOKEN_URL,
+		client_id = FIT_CLIENT_ID,
+		client_secret = FIT_CLIENT_SECRET,
+		code=authcode )
+
+	session['oauth_token'] = token
+
+	return render_template('okauth.html')
+
+
+def fetch_data(startbound, endbound, data_type):
+	"""Take a parameter from the caller as to what kind of data is desired,
+	and start/end time parameters (in nanoseconds, since the Google API was clearly 
+	written by Java programmers) for the desired dataset.
+
+	pass string 'speed' for speed data.
+	pass string 'bpm' for bpm data. """
 	
-# 	# Create an OAuth2 session with the token stored in the Flask session:
-# 	google = OAuth2Session(FIT_CLIENT_ID, token=session['oauth_token'])
+	# Create an OAuth2 session with the token stored in the Flask session:
+	google = OAuth2Session(FIT_CLIENT_ID, token=session['oauth_token'])
 	
-# 	# Checks what the desired datatype is:
-# 	if data_type == 'bpm':
-# 		source = BPM_DATA
-# 	elif data_type == 'speed':
-# 		source = SPEED_DATA
+	# Checks what the desired datatype is:
+	if data_type == 'bpm':
+		source = BPM_DATA
+	elif data_type == 'speed':
+		source = SPEED_DATA
 
-# 	# Use the caller-supplied parameters to build the HTTP request.
-# 	api_call = QUERY_BASE_URI + source + '/datasets' + '/' + startbound + '-' + endbound
-# 	api_response = google.get(api_call)
+	# Use the caller-supplied parameters to build the HTTP request.
+	api_call = QUERY_BASE_URI + source + '/datasets' + '/' + startbound + '-' + endbound
+	api_response = google.get(api_call)
 
-# 	print "From google: ----------", api_response
-# 	if api_response.status_code == 200:
-# 		return api_response.content
-# 		# TODO function returns a string; would like to optimize
-# 		# this by making it return a dict to avoid repetition 
-# 	elif api_response.status_code == 403:
-# 		return "This app hasn't been authorized to access your location or body sensor data." 
-# 	else:
-# 		print "API call returned ", api_response.status_code
-# 		return "There was a problem getting your data from Google. Please check your debug logs."
-# 	# import pdb; pdb.set_trace()
+	print "From google: ----------", api_response
+	if api_response.status_code == 200:
+		return api_response.content
+		# TODO function returns a string; would like to optimize
+		# this by making it return a dict to avoid repetition 
+	elif api_response.status_code == 403:
+		return "This app hasn't been authorized to access your location or body sensor data." 
+	else:
+		print "API call returned ", api_response.status_code
+		return "There was a problem getting your data from Google. Please check your debug logs."
+	# import pdb; pdb.set_trace()
 
 
 
